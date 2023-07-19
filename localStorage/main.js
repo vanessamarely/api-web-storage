4c8bc0db72e0829e7303d9ed49a6f70425eecf9f
Vanessa Aristizabal <vanessamarely@gmail.com>
2023-07-19 22:53:38 +0000

    comments added

--- localStorage/main.js
+++ localStorage/main.js
@@ -1,31 +1,37 @@
+// get the ul element
 const ul = document.querySelector('ul');
+// get the input element
 const input = document.getElementById('item');
+// get the add button
 const addButton = document.getElementById('addButton');
+// get the delete button
 const deleteButton = document.getElementById('deleteButton');
-let itemsArray = localStorage.getItem('items') ?
-JSON.parse(localStorage.getItem('items')) : [];
+//get the localStorage items
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
+//for each item in the localStorage, add it to the ul
 itemsArray.forEach(addTask);
 
+//add the new task to the ul and to the localStorage
 function addTask(text){
   const li = document.createElement('li')
   li.textContent = text;
   ul.appendChild(li);
 }
-
+//add the new task to the localStorage
 function add(){
   itemsArray.push(input.value);
   localStorage.setItem('items', JSON.stringify(itemsArray));
   addTask(input.value);
   input.value = '';
 }
-
+//delete all the tasks from the ul and from the localStorage
 function deleteAllTask(){
   localStorage.clear();
   ul.innerHTML = '';
   itemsArray = [];
 }
-
+//add the new task to the ul and to the localStorage when the add button is clicked
 addButton.addEventListener( 'click', add );
-
+//delete all the tasks from the ul and from the localStorage when the delete button is clicked
 deleteButton.addEventListener( 'click', deleteAllTask );
--- sessionStorage/main.js
+++ sessionStorage/main.js
@@ -1,9 +1,10 @@
+// create object student
 const student = {
   name: "John",
   age: 19,
   subject: "Javascript",
 };
-
+// create object tutorial
 const tutorial = {
   subject: "Javascript",
   cost: "Free",
@@ -13,10 +14,11 @@ const tutorial = {
 sessionStorage.setItem("student", JSON.stringify(student));
 // get data
 const data = sessionStorage.getItem("student");
-
+// show data
 document.getElementById("retrive").innerHTML = data;
 //create session storage
 sessionStorage.setItem("tutorial", JSON.stringify(tutorial));
 // get data
 var dataTutorial = sessionStorage.getItem("tutorial");
+// show data
 document.getElementById("retrive1").innerHTML = dataTutorial;
